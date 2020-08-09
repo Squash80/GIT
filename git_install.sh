18bb16603a975864749c648bfaca987ad8208cf5
Squash80 <68205506+Squash80@users.noreply.github.com>
2020-08-09 02:51:37 +0100

    Create git_install.sh

---
+++ git_install.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+echo $Green  
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is written by Stephin now start"
+echo "---------------------------------------------------------------------------------"
+
+echo "007"
+echo "007"
+echo "007"
+echo "."
+"-----------------------------------------------------------------------------------------"
+echo  
+echo                    " we are Hacker's "               
+ echo          " we are  Anonymous Tim3 "                                                                    
+echo   "-----------------------------------------------------------------------------------------"
+
+echo "007"
+echo "007"
+echo "007"
+
+echo "."
+pkg update
+pkg upgrade
+apt-get update -y                      #update in progress
+pkg install python -y           #installation......
+pkg install python2 -y
+pkg install ruby -y 
+pkg install git -y
+pkg install screenfetch -y
+pkg install cowsay -y
+pkg install toilet -y
+pkg install figlet -y
+echo  "------------------------------------------------------------------------------------------"
+echo   " u can now hack anything      "               
+ echo   " completing wait 3 2 1 "                                                                    
+echo   "####################################"
+
+pkg install php -y 
+pkg install perl -y
+pkg install nmap -y
+pkg install openssh -y
+echo  "#####################################"
+echo   "anynomous "               
+ echo   " join us on facebook Anonymous Tim3  "                                                                   echo   "####################################"
+
+pkg install bash -y
+pkg install clang -y
+pkg install jq -y
+pkg install macchanger -y
+
+echo " "                                                                   
+echo   "####################################"
+
+pkg install nano -y
+pkg install curl -y
+pkg install tar -y
+pkg install zip -y
+pkg install fish -y
+                                                            echo   " "
+                                                            
+pkg install unzip -y
+pkg install tor -y 
+pkg install sudo -y
+pkg install wget -y
+pkg install tor -y
+pkg install w3m -y
+pkg install cmatrix -y
+echo  "#####################################"
+echo   " we are hackers "               
+                                                                  echo   "####################################"
+
+pkg install wcalc -y 
+pkg install openssl
+pkg install bmon -y
+
+echo  "#####################################"
+               
+echo   " processing wait......
+"                                                                    
+echo   "####################################"
+
+echo " If You Connected To Internet Then u downloaded success fully subscribe us Anonymous  Tim3...we are Hacker's dont underestimate us "
+
+  echo              "Now your terminal is ready to use"
+
+exit
