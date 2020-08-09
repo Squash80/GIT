e8a0c3cebfdb82e695129eb1e6175ee9cdf7a091
Squash80 <68205506+Squash80@users.noreply.github.com>
2020-08-09 01:38:30 +0100

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,39 @@
+pkg install python
+pkg install python2
+pkg install git
+#for:P-TS
+git clone http://github.com/Ethical-Hacking-Zone/P-TS
+#for:lemon
+git clone http://github.com/D3VL/L3MON
+#for:Termux black
+git clone http://github.com/Hax4us/TermuxBlack
+#for:shell phish
+git clone http://github.com/cyberknight777/shellphish
+#for:SM brute
+git clone https://github.com/m4ll0k/SMBrute
+#for:sniff joke
+git clone https://github.com/vecna/sniffjoke
+#for:sniper
+git clone https://github.com/1N3/Sn1per
+#for:fbi
+git clone https://github.com/xHak9x/fbi.git
+#for:onex
+git clone https://github.com/rajkumardusad/onex.git
+#for:ADB toolkit
+git clone https://github.com/ASHWIN990/ADB-Toolkit
+#for:T-Lazy
+git clone https://github.com/Ethical-Hacking-Zone/T-Lazy.g>
+#for:TBomb
+git clone https://github.com/TheSpeedX/TBomb
+#for:zphisher
+git clone https://github.com/htr-tech/zphisher.git
+#for:DarkFly tool
+git clone https://github.com/Ranginang67/DarkFly-Tool
+#for:locator
+git clone https://github.com/Ethical-Hacking-Zone/locator
+#for:Hidden Eye
+git clone https://github.com/DarkSecDevelopers/HiddenEye
+#for:symbiote
+git clone https://github.com/404-ghost/symbiote
+#for:EHZ(phonespliot)
+git clone https://github.com/Ethical-Hacking-Zone/EHZ
