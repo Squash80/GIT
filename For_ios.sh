6cb007b741ebbeb32c404dfef8ba101202886d99
Squash80 <68205506+Squash80@users.noreply.github.com>
2023-04-28 18:23:26 +0100

    Create For_ios.sh

---
+++ For_ios.sh
@@ -0,0 +1,86 @@
+echo $Green  
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is written by Squash80 now start"
+echo "---------------------------------------------------------------------------------"
+
+echo "0"
+echo "00"
+echo "000"
+echo "."
+echo "-----------------------------------------------------------------------------------------"  
+echo                  "I am ultimate Hacker"               
+ echo          " I am Supernova_de_Klaxon "                                                                    
+echo   "-----------------------------------------------------------------------------------------"
+
+echo "000"
+echo "00"
+echo "0"
+
+echo "."                   #update in progress
+apk add python         #installation......
+apk add python2
+apk add ruby
+apk add git
+apk add screenfetch
+apk add cowsay
+apk add toilet 
+apk add figlet
+echo  "------------------------------------------------------------------------------------------"
+echo   " you can now hack anything      "               
+ echo   " completing wait 3 2 1 "                                                                    
+echo   "####################################"
+
+apk add  php
+apk add perl
+apk add nmap
+apk add openssh
+echo  "#####################################"
+echo   "SP"               
+ echo   " join my group on telegraph   https://t.me/EthicalHackingZone_EHZ "                                                                  echo   "####################################"
+
+apk add bash
+apk add clang
+apk add jq
+apk add macchanger
+
+echo " "                                                                   
+echo   "####################################"
+
+apk add nano 
+apk add curl
+apk add tar
+apk add zip
+apk add fish
+                                                            echo   " "
+apk add sl
+apk add coreutils                                                            
+apk add  unzip 
+apk add tor
+apk add sudo 
+apk add wget
+apk add tor
+apk add w3m
+apk add cmatrix 
+echo  "#####################################"
+echo            "I am ultimate hacker "               
+                                                                  echo   "####################################"
+apk add  pulseaudio 
+apk add vim
+apk add nodes
+apk add glib-bin
+apk add wcalc
+apk add openssl
+apk add bmon
+clear
+echo  "#####################################"
+               
+echo   " processing wait......
+"                                                                    
+echo   "####################################"
+
+echo " If You Connected To Internet Then you downloaded successfully...I am ultimate hacker dont underestimate me"
+
+  echo              "Now your terminal is ready to use"
+
+exit
+sl
