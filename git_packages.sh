e2153f78223148725c06a07b695bb4f057d46840
Squash80 <68205506+Squash80@users.noreply.github.com>
2024-08-02 01:34:07 +0100

    Update git_packages.sh

--- git_packages.sh
+++ git_packages.sh
@@ -18,63 +18,63 @@ echo "00"
 echo "0"
 
 echo "."
-apt update
-apt upgrade
-apt-get update -y                      #update in progress
-apt install python -y           #installation......
-apt install python2 -y
-apt install ruby -y 
-apt install git -y
-apt install screenfetch -y
-apt install cowsay -y
-apt install toilet -y
-apt install figlet -y
+sudo apt update
+sudo apt upgrade
+sudo apt-get update -y                      #update in progress
+sudo apt install python -y           #installation......
+sudo apt install python2 -y
+sudo apt install ruby -y 
+sudo apt install git -y
+sudo apt install screenfetch -y
+sudo apt install cowsay -y
+sudo apt install toilet -y
+sudo apt install figlet -y
 echo  "------------------------------------------------------------------------------------------"
 echo   " you can now hack anything      "               
  echo   " completing wait 3 2 1 "                                                                    
 echo   "####################################"
 
-apt install php -y 
-apt install perl -y
-apt install nmap -y
-apt install openssh -y
+sudo apt install php -y 
+sudo apt install perl -y
+sudo apt install nmap -y
+sudo apt install openssh -y
 echo  "#####################################"
 echo   "SP"               
  echo   " join my group on telegraph   https://t.me/EthicalHackingZone_EHZ "                                                                  echo   "####################################"
 
-apt install bash -y
-apt install clang -y
-apt install jq -y
-apt install macchanger -y
+sudo apt install bash -y
+sudo apt install clang -y
+sudo apt install jq -y
+sudo apt install macchanger -y
 
 echo " "                                                                   
 echo   "####################################"
 
-apt install nano -y
-apt install curl -y
-apt install tar -y
-apt install zip -y
-apt install fish -y
+sudo apt install nano -y
+sudo apt install curl -y
+sudo apt install tar -y
+sudo apt install zip -y
+sudo apt install fish -y
                                                             echo   " "
-apt install sl -y
-apt install coreutils -y                                                            
-apt install unzip -y
-apt install tor -y 
-apt install sudo -y
-apt install wget -y
-apt install tor -y
-apt install w3m -y
-apt install cmatrix -y
+sudo apt install sl -y
+sudo apt install coreutils -y                                                            
+sudo apt install unzip -y
+sudo apt install tor -y 
+sudo apt install sudo -y
+sudo apt install wget -y
+sudo apt install tor -y
+sudo apt install w3m -y
+sudo apt install cmatrix -y
 echo  "#####################################"
 echo            "I am ultimate hacker "               
                                                                   echo   "####################################"
-apt install pulseaudio 
-apt install vim
-apt install nodes
-apt install glib-bin
-apt install wcalc -y 
-apt install openssl
-apt install bmon -y
+sudo apt install pulseaudio 
+sudo apt install vim
+sudo apt install nodes
+sudo apt install glib-bin
+sudo apt install wcalc -y 
+sudo apt install openssl
+sudo apt install bmon -y
 clear
 echo  "#####################################"
                
